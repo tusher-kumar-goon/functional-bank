1e6163fc593c390b9653c017f367751960cbb77a
tushergoon <tushergoondip@gmail.com>
2021-08-17 19:06:42 +0600

    first comment

---
+++ js/bank.js
@@ -0,0 +1,19 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    //  check email and password
+    if (userEmail == 'sontan@baap.com' && userPassword == 'secret') {
+        window.location.href = 'banking.html'
+    }
+})
+
+// handle deposite button event
+document.getElementById("deposit-button").addEventListener
+    ('click', function () {
+        console.log('deposite hoitase');
+    })
+
